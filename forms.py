fc76b02dc8106db67638594f7208ce48c71811f3
Grant <granther321@gmail.com>
2024-10-25 06:15:13 +0000

    Fixed typo in forms, edit->delete in component

--- forms.py
+++ forms.py
@@ -16,7 +16,7 @@ class SelectProfileForm(FlaskForm):
 class ModPackForm(FlaskForm):
     name = StringField('Modpack Name', validators=[DataRequired()], render_kw={"class": "border border-black rounded-lg text-black px-2 py-1 focus:outline-none w-full text-lg", "autocomplete":"off"})
     mods = QuerySelectMultipleFieldWithChecks("Mods")
-    submit = SubmitField('Sumbit', render_kw={"class": "bg-sky-500 hover:bg-sky-700 text-black py-2 px-5 rounded-full font-bold text-md transition duration-300"})
+    submit = SubmitField('Submit', render_kw={"class": "bg-sky-500 hover:bg-sky-700 text-black py-2 px-5 rounded-full font-bold text-md transition duration-300"})
 
 class NewProfileForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()], render_kw={"class": "border border-black rounded-lg text-black px-2 py-1 focus:outline-none w-full text-lg", "autocomplete":"off"})
